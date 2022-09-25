6f7d800b46a431e5b33af50d6c1097d35a79087c
Brian Kwendo <bayokwendo@gmail.com>
2022-09-25 03:39:11 +0300

    new file for bustabit domi

--- web_serve/config/config.js
+++ web_serve/config/config.js
@@ -11,7 +11,7 @@ if (production) {
 }
 module.exports = {
   PRODUCTION: production,
-  DATABASE_URL: "postgres://supabasta:xAs9cMddjjv#@CNu_fEGg@54.217.120.10/supabastadb",
+  DATABASE_URL: "postgres://supabasta:xAs9cMVZSRPWGCNufEGg@localhost/supabastadb",
   BIP32_DERIVED:
     "xpub6AHA9hZDN11k2ijHMeS5QqHx2KP9aMBRhTDqANMnwVtdyw2TDYRmF8PjpvwUFcL1Et8Hj59S3gTSMcUQ5gAqTz3Wd8EsMTmF3DChhqPQBnU",
   AWS_SES_KEY: process.env.AWS_SES_KEY,
