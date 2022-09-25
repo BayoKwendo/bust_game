074ac41fe4687bb668136363553535c639fd571b
Brian Kwendo <bayokwendo@gmail.com>
2022-09-25 03:54:41 +0300

    new file for bustabit domi

--- web_serve/client_new/scripts/game-logic/engine.js
+++ web_serve/client_new/scripts/game-logic/engine.js
@@ -16,10 +16,10 @@ define([
      */
     _.extend(this, Events);
 
-    self.ws = io("https://luckybox.ng:3844/");
+    // self.ws = io("https://luckybox.ng:3844/");
     // self.ws = io(AppConstants.Engine.HOST);
 
-    // self.ws = io("http://localhost:3846/");
+    self.ws = io("http://localhost:3846/");
 
     //Dev functions
     //window.disconnect = function() {
